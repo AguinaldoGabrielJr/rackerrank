23d81e94171570a9c565bd98c0634e3b2c492f68
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-11 18:08:38 -0300

    Java-stdin-stdout II

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,9 +1,23 @@
 package hackerrank;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(bufferedReader.readLine().trim());
+
+		double d = Double.parseDouble(bufferedReader.readLine().trim());
+
+		String s = bufferedReader.readLine();
+
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + num);
+
+		bufferedReader.close();
+	}
 }
