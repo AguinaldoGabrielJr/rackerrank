d8fb891ab2c49a3b1fc594a5cee7aa1b8e92ce0e
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-12 16:06:15 -0300

    java-int-to-string

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -5,34 +5,21 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 
-		int area;
-
 		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		if (n >= -100 && n <= 100) {
 
-		int b = sc.nextInt();
-		int h = sc.nextInt();
-
-		if (b <= 100 && b >= -100) {
-			if (h <= 100 && h >= -100) {
+			try {
+				String convertido = Integer.toString(n);
+				System.out.println("Good job");
+			} catch (Exception e) {
+				System.out.println("Wrong answer");
 				
-				area = b * h;
-				if (b < 0 && h < 0) {
-					area = b * h;
-				
-					if (area > 0) {
-						area *= -1;
-
-					}
-				}
-				if (area > 0) {
-					System.out.println(area);
-				} else {
-					System.out.println("java.lang.Exception: Breadth and height must be positive");
-				}
-
 			}
-
 		}
+		
 		sc.close();
 	}
 }
