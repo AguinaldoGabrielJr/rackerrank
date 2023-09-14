e6d4b934184ecc366f8e58c821fccfeb4e48d492
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-14 10:37:06 -0300

    java-date-and-time

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,40 +1,26 @@
 package hackerrank;
 
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.util.Calendar;
 import java.util.Scanner;
 
+public class Solution{
+public static void main(String[] args){
 
-public class Solution {
-	public static void main(String[] args){
-	
-		Scanner sc = new Scanner(System.in);
-		sc.useLocale(Locale.ENGLISH);
-		
-		Locale indiaLocale = new Locale("en", "IN");
-		
-		double valor = sc.nextDouble();
-		
-		if (valor >= 0 && valor <= 1000000000) {
-			
-			NumberFormat nfUsa = NumberFormat.getCurrencyInstance(Locale.US);
-			String valorUsa = nfUsa.format(valor);
-			System.out.println("US: " + valorUsa);
+    Scanner sc = new Scanner(System.in);
 
-			NumberFormat nfIndia = NumberFormat.getCurrencyInstance(indiaLocale);
-			String valorIndia = nfIndia.format(valor);
-			System.out.println("India: " + valorIndia);
+    int month = sc.nextInt();
+    int day= sc.nextInt();
+    int year= sc.nextInt();
 
-			NumberFormat nfChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
-			String valorChina = nfChina.format(valor);
-			System.out.println("China: " + valorChina);
+    Calendar calendar = Calendar.getInstance();
 
-			NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-			String valorFrance = nfFrance.format(valor);
-			System.out.println("France: " + valorFrance);
-		}
-		
-		sc.close();
-	
-	}
+    calendar.set(year,month-1, day);
+
+    int dayOfWeek= calendar.get(Calendar.DAY_OF_WEEK);
+
+    String[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+
+    System.out.println(daysOfWeek[dayOfWeek -1]);
+
+}
 }
