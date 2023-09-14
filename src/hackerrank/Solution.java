cdad1b1382f7a8606f1694b276f403a59f6f879c
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-14 10:11:20 -0300

    java-currency-formatter(java15nfuncionaporcausadeumsimbolodiferente)

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,25 +1,40 @@
 package hackerrank;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Solution {
-	public static void main(String[] args) {
-
+	public static void main(String[] args){
+	
 		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.ENGLISH);
 		
-		int n = sc.nextInt();
+		Locale indiaLocale = new Locale("en", "IN");
 		
-		if (n >= -100 && n <= 100) {
+		double valor = sc.nextDouble();
+		
+		if (valor >= 0 && valor <= 1000000000) {
+			
+			NumberFormat nfUsa = NumberFormat.getCurrencyInstance(Locale.US);
+			String valorUsa = nfUsa.format(valor);
+			System.out.println("US: " + valorUsa);
 
-			try {
-				String convertido = Integer.toString(n);
-				System.out.println("Good job");
-			} catch (Exception e) {
-				System.out.println("Wrong answer");
-				
-			}
+			NumberFormat nfIndia = NumberFormat.getCurrencyInstance(indiaLocale);
+			String valorIndia = nfIndia.format(valor);
+			System.out.println("India: " + valorIndia);
+
+			NumberFormat nfChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
+			String valorChina = nfChina.format(valor);
+			System.out.println("China: " + valorChina);
+
+			NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+			String valorFrance = nfFrance.format(valor);
+			System.out.println("France: " + valorFrance);
 		}
 		
 		sc.close();
+	
 	}
 }
