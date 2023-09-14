a44ee00339adb54c14d51d7a73b15bb9b6a43c0d
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-14 11:03:55 -0300

    java-strings-introduction

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,26 +1,27 @@
 package hackerrank;
-
-import java.util.Calendar;
 import java.util.Scanner;
 
-public class Solution{
-public static void main(String[] args){
+public class Solution {
+    public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-
-    int month = sc.nextInt();
-    int day= sc.nextInt();
-    int year= sc.nextInt();
-
-    Calendar calendar = Calendar.getInstance();
-
-    calendar.set(year,month-1, day);
-
-    int dayOfWeek= calendar.get(Calendar.DAY_OF_WEEK);
-
-    String[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
-
-    System.out.println(daysOfWeek[dayOfWeek -1]);
-
-}
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        
+        int compareTo = A.compareTo(B);
+        int lengthA = A.length();
+        int lengthB = B.length();
+        
+        System.out.println(lengthA + lengthB);
+        if (compareTo > 0) {
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+        
+        System.out.print(A.substring(0,1).toUpperCase()+ A.substring(1) + " ");
+        System.out.println(B.substring(0,1).toUpperCase()+ B.substring(1));
+        
+        sc.close();
+    }
 }
