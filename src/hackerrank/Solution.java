fd45fcd7bb93f559ca0394d598fd8d9e74501f5c
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-09-15 16:18:30 -0300

    java-generics

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,31 +1,35 @@
 package hackerrank;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.reflect.Method;
+
+class Printer {
+	public static <T> void printArray(T[] array) {
+		for (T element : array) {
+			System.out.println(element + " ");
+		}
+	}
+
+}
 
 public class Solution {
-	public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
-		int testCases = Integer.parseInt(in.nextLine());
+	public static void main(String args[]) {
+		Printer myPrinter = new Printer();
+		Integer[] intArray = { 1, 2, 3 };
+		String[] stringArray = { "Hello", "World" };
+		myPrinter.printArray(intArray);
+		myPrinter.printArray(stringArray);
+		int count = 0;
 
-		while (testCases > 0) {
+		for (Method method : Printer.class.getDeclaredMethods()) {
+			String name = method.getName();
 
-			String line = in.nextLine();
-			Pattern pattern = Pattern.compile("<([^<>/]+)>([^<>]+)</(\\1)>");
-			Matcher matcher = pattern.matcher(line);
-			if (matcher.find()) {
-				System.out.println(matcher.group(2));
-				while (matcher.find()) {
-					System.out.println(matcher.group(2));
-				}
-			} else {
-				System.out.println("None");
-			}
-			testCases--;
-
+			if (name.equals("printArray"))
+				count++;
 		}
-		in.close();
+
+		if (count > 1)
+			System.out.println("Method overloading is not allowed!");
+
 	}
 }
